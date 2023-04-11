ade8c31560696c97e92add9c576544b7995870a9
Simone Rotondi <rotondi97simone@gmai.com>
2023-04-12 00:21:07 +0200

    Initial commit

---
+++ Matlab/Cheaser.m
@@ -0,0 +1,57 @@
+function [dy,u] = Cheaser(t,y,y_goal)
+    if ~exist('y','var')
+        Return_lqr_values()
+        return;
+
+    end
+    y=y(1:6);
+
+
+    u=get_k()*(y_goal-y);
+    dy = Sat_Translational_Dyn(t,y,u);
+end
+
+%%Helping Functions
+function K=get_k()
+    %K=lqr(get_A(),get_B(),get_Q(),get_R());
+    %pause
+    K=[...
+        3.1623   -0.0000   -0.0000    3.1633    0.0000    0.0000;...
+        0.0000    3.1623    0.0000   -0.0000    3.1633    0.0000;...
+        0.0000   -0.0000    3.1623    0.0000   -0.0000    3.1633;];
+end
+
+function A=get_A()
+    n= 6.3135e-04;
+    %Init Vars using     %F_LV_LH
+    A=[ 0 0 0 1 0 0 ;...
+        0 0 0 0 1 0;...
+        0 0 0 0 0 1;...
+        3*n^2 0 0 0 2*n 0;...
+        0 0 0 -2*n 0 0;...
+        0 0 -n^2 0 0 0;...
+        ];
+end
+function B= get_B()
+    B=[ 0 0 0;...
+        0 0 0;...
+        0 0 0;...
+        1 0 0;...
+        0 1 0;...
+        0 0 1;];
+end
+function Q=get_Q()
+    Q=diag([1,1,1,1,1,1]);
+end
+function R=get_R()
+    R=diag([1,1,1])*1e-7;
+end
+function Return_lqr_values()
+    K=lqr(get_A(),get_B(),get_Q(),get_R());
+    %string([K,[";...";";...";";];"]])
+    NoInput="No Input has been provided, the K lqr values are returned ";
+    disp(NoInput)
+    disp(K)
+end
+
+

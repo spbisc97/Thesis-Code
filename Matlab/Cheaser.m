b8192554719418e2736266393cc5f5e05ce0f41f
Simone Rotondi <rotondi97simone@gmail.com>
2023-04-26 11:02:52 +0200

    Fix Some control issues

--- Matlab/Cheaser.m
+++ Matlab/Cheaser.m
@@ -8,8 +8,6 @@ function [dy,u] = Cheaser(t,y,y_goal) %#codegen
     y=y(:);
 
 
-    
-
     u_tranlational=translation_control(y(1:6),y_goal(1:6));%later mass will have to be passed
     u_attitude=attitude_control(y(7:13),y_goal(7:13));%later mass will have to be passed
 
@@ -24,8 +22,8 @@ end
 %% Control Functions
 function [u,e]=translation_control(y,y_goal)
     %u=0;e=0;
-    e=y_goal(1:6)-y(1:6);
-    u=lqr_control_tra(e);
+    %e=y_goal(1:6)-y(1:6);
+    [u,e]=lqr_control_tra(y(1:6),y_goal(1:6));
 end
 
 function [u,e]=attitude_control(y,y_goal)
@@ -36,49 +34,66 @@ function [u,e]=attitude_control(y,y_goal)
 end
 
 %% Translation Helping Functions
-function u=lqr_control_tra(error)
-    u=get_k_tra()*(error);
+function [u,e]=lqr_control_tra(y,y_goal)
+    e=y_goal(1:6)-y(1:6);
+    u=get_k_tra(y,[0;0;0])*(e);
+    
 end
 
-function K=get_k_tra()
-    %K=lqr(get_A_tra(),get_B_tra(),get_Q_tra(),get_R_tra());
+function K=get_k_tra(y,u)
+    [A,B]=get_AB_tra(y,u);
+    K=lqr(A,B,get_Q_tra(),get_R_tra());
     %pause
-    K=[...
-        3.1623   -0.0000   -0.0000    3.1633    0.0000    0.0000;...
-        0.0000    3.1623    0.0000   -0.0000    3.1633    0.0000;...
-        0.0000   -0.0000    3.1623    0.0000   -0.0000    3.1633;];
+%    K=[...
+%    99.9999446137011        -0.107264213962748      3.44547485353584e-14           89.498578587015     -8.00054467120503e-15      2.95056468803997e-14;...
+%    0.107264213962668          99.9999446137011     -6.04285394796429e-14       2.1316282072803e-14           89.498578587015     -9.52252925846075e-15;...
+% -1.4019034670654e-14      7.00884783457919e-14                       100     -9.53985852357144e-15      7.69306494634369e-14          89.4985478828506];
 end
 
-function A=get_A_tra()
-    n= 6.3135e-04;
+function [A,B]=get_AB_tra(y,u)
     %Init Vars using     %F_LV_LH
-    A=[ 0 0 0 1 0 0 ;...
-        0 0 0 0 1 0;...
-        0 0 0 0 0 1;...
-        3*n^2 0 0 0 2*n 0;...
-        0 0 0 -2*n 0 0;...
-        0 0 -n^2 0 0 0;...
-        ];
+    parameters=Sat_params();
+    mu=parameters.mu;
+    rt= parameters.rt ;%m r of the targhet
+    n=parameters.n;
+    y(7)=10;
+
+
+
+    A=[0,             0, 0,   1,    0,        0;...%                 0;
+       0,             0, 0,   0,    1,        0;...%                        0;
+       0,             0, 0,   0,    0,        1;...%                        0;
+       3*n^2,          0, 0,   0,  2*n,        0;...%        -u(1)/(parameters.mass + y(7))^2;
+       0,             0, 0,-2*n,    0,        0;...%        -u(2)/(parameters.mass + y(7))^2;
+       0,             0, -n^2,   0,    0,        0];...%        -u(3)/(parameters.mass + y(7))^2;
+      % 0,             0, 0,   0,    0,        0];%                        1];
+
+       B=[            0,             0,             0;
+            0,             0,             0;
+            0,             0,             0;
+1/(parameters.mass + y(7)),             0,             0;
+            0, 1/(parameters.mass + y(7)),             0;
+            0,             0, 1/(parameters.mass + y(7))];
+         %   0,             0,             0];
+
+    A=A(1:6,1:6);
+    B=B(1:6,:);
+
 end
-function B= get_B_tra()
-    B=[ 0 0 0;...
-        0 0 0;...
-        0 0 0;...
-        1 0 0;...
-        0 1 0;...
-        0 0 1;];
-end
+
 function Q=get_Q_tra()
-    Q=diag([1,1,1,1,1,1]);
+    Q=diag([1e1,1e1,1e1,0.1,0.1,0.1]*1e-3);
 end
 function R=get_R_tra()
-    R=diag([1,1,1])*1e-7;
+    R=diag([1,1,1])*1e14;
 end
 function Return_lqr_values()
-    K=lqr(get_A_tra(),get_B_tra(),get_Q_tra(),get_R_tra());
+    [A,B]=get_AB_tra([0;0;0;0;0;0;10],[0;0;0]);
+    K=lqr(A,B,get_Q_tra(),get_R_tra());
     %string([K,[";...";";...";";];"]])
     NoInput="No Input has been provided, the K lqr values are returned ";
     disp(NoInput)
+    format longg
     disp(K)
 end
 

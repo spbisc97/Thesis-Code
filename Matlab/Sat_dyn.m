4800a5cd7ddb8296ca0d61bcb5b744fe65f1ce20
Simone Rotondi <rotondi97simone@gmail.com>
2023-04-26 11:01:59 +0200

    Add mass also for attitude

--- Matlab/Sat_dyn.m
+++ Matlab/Sat_dyn.m
@@ -5,7 +5,7 @@ function [dy,u]=Sat_dyn(t,y,u_tranlational,u_attitude)
 
 
     [dy_tra,u_tranlational] = Sat_Translational_Dyn(t,[y(1:6);mass],u_tranlational);
-    [dy_att,u_attitude]= Sat_Attitude_Dyn(t,y(7:13),u_attitude);
+    [dy_att,u_attitude]= Sat_Attitude_Dyn(t,[y(7:13);mass],u_attitude);
     dy_mass=dy_att(8)+dy_tra(7);
 
     dy=[dy_tra(1:6);dy_att(1:7);dy_mass];

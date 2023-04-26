d93264428a0b8dbd35800d60b2a1ec3ac9e5b90f
Simone Rotondi <rotondi97simone@gmail.com>
2023-04-26 10:59:39 +0200

    Add saturation to control

--- Matlab/Sat_Attitude_Dyn.m
+++ Matlab/Sat_Attitude_Dyn.m
@@ -3,17 +3,20 @@ function [dy,u] = Sat_Attitude_Dyn(~,y,u) %#codegen
     %   The output of this function will be 7 elements
     %   quaternion+velocities
 
-
-    parameters.Isp=2250;
-    parameters.Tmax=1.05e-7;
+    parameters=Sat_params();
+    
     g=9.81;
-    u=min(parameters.Tmax,max(-parameters.Tmax,u(:)));
-    torques=u;
 
     q=y(1:4);
     w=y(5:7);
-    I = diag([0.0023, 0.0023, 0.0023]); % assume a uniform density and a 10cm cube shape
-    invI=inv(I); %calculated a priori in future implementations
+
+    %mass=y(8);
+    u=min(parameters.Tmax,max(-parameters.Tmax,u(:)))*min(1,max(0,y(8)*1e2));
+    torques=u;
+
+    
+    % assume a uniform density and a 10cm cube shape
+    %calculated a priori in future implementations
     %if ~isnumeric(y(1))
     % syms Ix Iy Iz
     % I = diag([Ix, Iy, Iz]);
@@ -29,7 +32,7 @@ function [dy,u] = Sat_Attitude_Dyn(~,y,u) %#codegen
     %cross(w,ang_momentum), always zero if inertia is diagonal
 
 
-    w_dot=[invI * (-cross(w(1:3), I * w(1:3)) + torques)]; %#ok
+    w_dot=[parameters.invI * (-cross(w(1:3), parameters.I * w(1:3)) + torques)]; %#ok
 
     m_dot=-sum(abs(u(:)))/(g*parameters.Isp);
 

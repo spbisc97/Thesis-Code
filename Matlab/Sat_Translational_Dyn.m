c6683b27c8ab1947326102c7b0e4fa019d276efb
Simone Rotondi <rotondi97simone@gmail.com>
2023-04-26 11:00:59 +0200

    Saturation + parameters from outside

--- Matlab/Sat_Translational_Dyn.m
+++ Matlab/Sat_Translational_Dyn.m
@@ -1,11 +1,11 @@
 function [dy,u] = Sat_Translational_Dyn(~,y,u) %#codegen
 
-    parameters.Isp=2250;
-    parameters.Tmax=1.05e-7;
+    parameters=Sat_params();
+
     g=9.81;
 
     %Takes u directly as acc
-    u=min(parameters.Tmax,max(-parameters.Tmax,u(:)));
+    u=min(parameters.Tmax,max(-parameters.Tmax,u(:)))*min(1,max(0,y(7)*1e2));;
     torques=u;
     y=y(:);
     %n_dot=0;
@@ -13,27 +13,33 @@ function [dy,u] = Sat_Translational_Dyn(~,y,u) %#codegen
 
     %y 1to6 position and velocity
     %y 7 is mass
-    
-    mass=y(7);
 
     %set for now
     mu=3.986004418*10^14;
+    %syms mu
     rt= 6.6*1e6 ;%m r of the targhet
+    %syms rt
     rc=norm([rt+y(1);y(2);y(3)]);
+    rc=((rt+y(1))^2+y(2)^2+y(3)^2)^(1/2);
+    %syms rc
     %~GPS are 2,66e7 m
     %n=sqrt(mu/rt^3);
-    n= 1.2e-3;
+    n= parameters.n;
+    %syms n
     eta=mu/(rc^3);
+    total_mass=(y(7)+parameters.mass);
 
     
     %Init Vars using     %F_LV_LH
     dy(1)=y(4);
     dy(2)=y(5);
     dy(3)=y(6);
-    dy(4)=n^2*y(4)+2*n*y(5)+mu/(rt^2)-(eta)*(rt+y(1))+torques(1)/mass;
-    dy(5)=-2*n*y(5)+n^2*y(2)-(eta)*y(2)+torques(2)/mass;
-    dy(6)=-(eta)*y(6)+torques(3)/mass;
-
+    dy(4)=n^2*y(1)+2*n*y(5)+mu/(rt^2)-(eta)*(rt+y(1))+torques(1)/total_mass;
+    dy(5)=-2*n*y(4)+n^2*y(2)-(eta)*y(2)+torques(2)/total_mass;
+    dy(6)=-(eta)*y(3)+torques(3)/total_mass;
     dy(7)=-sum(abs(u(:)))/(g*parameters.Isp);
 
 end
+
+
+

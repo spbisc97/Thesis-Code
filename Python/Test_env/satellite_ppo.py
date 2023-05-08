889cb45fcea9817d4236c6270d186d526fc041e3
Simone Rotondi <rotondi97simone@gmail.com>
2023-05-08 15:05:56 +0200

    discrete

--- Python/Test_env/satellite_ppo.py
+++ Python/Test_env/satellite_ppo.py
@@ -1,21 +1,8 @@
-import numpy as np
-import os
+import matplotlib.pyplot as plt
 
-# from PyQt5.QtCore import QLibraryInfo # others imports
-# os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = QLibraryInfo.location(
-#     QLibraryInfo.PluginsPath
-# )
-# os.environ['OPENCV_LOG_LEVEL'] = 'SILENT'
-
-# import cv2
-# img = np.zeros((500,500,3),dtype='uint8')
-# cv2.imshow('Test',img);
-# cv2.waitKey(10);
-# cv2.destroyAllWindows();
-
-#prequel
-#needed for cv2.imshow to work
-
+plt.legend()
+plt.show(block=False)
+plt.close("all")
 
 
 from stable_baselines3 import PPO
@@ -29,18 +16,17 @@ import time
 import ffmpegio
 
 
-file_path=os.path.dirname(__file__)
+file_path = os.path.dirname(__file__)
 os.chdir(file_path)
 
-env_name = "Satellite-v0"
-#env = gym.make(env_name)
+env_name = "Satellite-discrete-v0"
+# env = gym.make(env_name)
 
 Algo = PPO
-Algo.name="PPO"
+Algo.name = "PPO"
 
 
-
-models_dir=f"models/{env_name}/{Algo.name}"
+models_dir = f"models/{env_name}/{Algo.name}"
 logdir = f"logs/{env_name}/{Algo.name}"
 imgs_dir = f"imgs/{env_name}/{Algo.name}"
 
@@ -48,43 +34,88 @@ os.makedirs(models_dir, exist_ok=True)
 os.makedirs(logdir, exist_ok=True)
 os.makedirs(imgs_dir, exist_ok=True)
 
+
+def run_episode(model, env_name, model_name="PPO", model_num=0, model_timesteps=0):
+    term = False
+    env=gym.make(env_name)
+    obs, info = env.reset()
+    rewards = [0]
+    counter = [0]
+    obss = [obs[0:6]]
+    actions = [env.action_space.sample() * 0]
+    while not term:
+        action, _states = model.predict(obs)
+        obs, reward, term, trunc, info = env.step(action)
+
+        rewards.append(reward)
+        counter.append(counter[-1] + 1)
+        obss.append(obs[0:6])
+        actions.append(action)
+
+        if term or trunc:
+            term = False
+            break
+    save_plot(
+        counter,
+        rewards,
+        obss,
+        actions,
+        name=model_name,
+        model_num=model_num,
+        model_timesteps=model_timesteps,
+    )
+
+
+def save_plot(
+    counter, rewards, obss, actions, name="PPO", model_num=0, model_timesteps=0
+):
+    rewards_sum = np.zeros_like(rewards)
+    for i in range(len(rewards)):
+        rewards_sum[i] = np.sum(rewards[:i])
+    fig, ((ax1, ax2), (ax3,ax4)) = plt.subplots(2, 2)
+
+    ax1.plot(counter, rewards, label="PPO")
+
+    ax2.plot(counter, obss)
+    ax2.legend(["x", "y", "z", "vx", "vy", "vz"])
+
+    ax3.plot(counter, actions)
+    ax3.legend(["ux", "uy", "uz"])
+    
+    ax4.plot(counter, rewards_sum)
+    ax4.legend(["PPO"])
+
+    plt.savefig(f"{imgs_dir}/{name}_{model_num}_{model_timesteps:.1e}.png")
+    plt.close("all")
+
+
 env = make_vec_env(env_name, n_envs=1)
-#env=gym.make(env_name)
+# env=gym.make(env_name)
 
 
-
-TIMESTEPS = 100_000
+TIMESTEPS = 50_000
 last_model = 0
-if last_model>0:
-    model=Algo.load(f"{models_dir}/{Algo.name}_{last_model}",env=env, verbose=1, tensorboard_log=logdir)
+if last_model > 0:
+    model = Algo.load(
+        f"{models_dir}/{Algo.name}_{last_model}",
+        env=env,
+        verbose=1,
+        tensorboard_log=logdir,
+    )
 else:
     input("Press Enter to delete logs and models")
     send2trash.send2trash(f"{logdir}/")
     send2trash.send2trash(f"{models_dir}/")
-    model=Algo('MlpPolicy', env, verbose=1, tensorboard_log=logdir)
-    
-episodes=100
-for i in range(last_model+1,last_model+episodes+1):
-    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False,tb_log_name=f"run_{i}")
+    model = Algo("MlpPolicy", env, verbose=1, tensorboard_log=logdir)
+
+episodes = 300
+for i in range(last_model + 1, last_model + episodes + 1):
+    model.learn(
+        total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name=f"run_{i}",log_interval=1
+    )
     model.save(f"{models_dir}/{Algo.name}_{i}")
-    last_model=i
-    
-env=gym.make(env_name)
+    last_model = i
+    run_episode(model, env_name=env_name, model_name=Algo.name, model_num=last_model, model_timesteps=model.num_timesteps)
 
-episodes = 1
-term=False
-for episode in range(1, episodes+1):
-    obs,info=env.reset()
-    
-    while not term:
-        action,_states =model.predict(obs)
-        obs,reward,term,trunc,info= env.step(action)
-        
-        if np.linalg.norm(obs[0:6])<1:
-            print(action)
-            print(reward)
-            print(obs)
 
-        if term or trunc:
-            term=False
-            break
+

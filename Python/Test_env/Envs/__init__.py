10915f6471b3efa2020777aee654ba77bb511e0b
Simone Rotondi <rotondi97simone@gmail.com>
2023-05-08 21:42:17 +0200

    add discrete

--- Python/Test_env/Envs/__init__.py
+++ Python/Test_env/Envs/__init__.py
@@ -1,19 +1,19 @@
 from gymnasium.envs.registration import register
 
 register(
-    id='Snake-v0',
-    entry_point='Envs.Snake_v0:SnakeEnv',
+    id="Snake-v0",
+    entry_point="Envs.Snake_v0:SnakeEnv",
 )
 register(
-    id='Satellite-v0',
-    entry_point='Envs.Satellite:Satellite_base',
+    id="Satellite-v0",
+    entry_point="Envs.Satellite:Satellite_base",
     max_episode_steps=20000,
     reward_threshold=25000.0,
 )
 register(
-    id='Satellite-discrete-v0',
-    entry_point='Envs.Satellite:Satellite_base',
-    max_episode_steps=20000,
+    id="Satellite-discrete-v0",
+    entry_point="Envs.Satellite:Satellite_base",
+    max_episode_steps=15000,
     reward_threshold=25000.0,
-    kwargs={'action_space': 'discrete'},
+    kwargs={"action_space": "discrete"},
 )

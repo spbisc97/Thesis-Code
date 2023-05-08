6be9c78d2847d09b919b41da8a743aaf0e94a617
Simone Rotondi <rotondi97simone@gmail.com>
2023-05-08 15:05:13 +0200

    add discrete box  action space

--- Python/Test_env/Envs/Satellite.py
+++ Python/Test_env/Envs/Satellite.py
@@ -6,33 +6,47 @@ import random
 
 
 class Satellite_base(gym.Env):
-    metadata = {'render_modes': ['rgb_array', 'human'],
-                'observation_spaces': ['MlpPolicy', 'MultiInputPolicy'], 
-                'control': ['Ml', 'ModelPredictiveControl', 'LQR', 'PID', 'Random', 'Human']}
+    metadata = {
+        "render_modes": ["rgb_array", "human"],
+        "observation_spaces": ["MlpPolicy", "MultiInputPolicy"],
+        "control": ["Ml", "ModelPredictiveControl", "LQR", "PID", "Random", "Human"],
+        "action_spaces": ["continuous", "discrete"],
+    }
 
-    def __init__(self, render_mode=None, observation_space="MlpPolicy",control="Ml"):
+    def __init__(self, render_mode=None, observation_space="MlpPolicy",action_space="continuous", control="Ml"):
         super(Satellite_base, self).__init__()
         # define action and observation space being gymnasium.spaces
-        assert control in self.metadata['control']
+        assert control in self.metadata["control"]
         self.control = control
         
-        self.action_space = spaces.Box(
-            low=-1, high=1, shape=(3,), dtype=np.float32)
+        assert action_space in self.metadata["action_spaces"]
+        if action_space == "continuous":        
+            self.action_space = spaces.Box(low=-1, high=1, shape=(3,), dtype=np.float32)
+        else:
+            self.action_space = spaces.Box(low=-1, high=1, shape=(3,), dtype=np.int8)
         #   or action_space in self.metadata['action_spaces']
         #   could be  better to use normalized action space
-        assert observation_space in self.metadata['observation_spaces']
-        if observation_space == 'MlpPolicy':
+        assert observation_space in self.metadata["observation_spaces"]
+        if observation_space == "MlpPolicy":
             self.observation_space = spaces.Box(
-                low=-np.inf, high=np.inf, shape=(7,), dtype=np.float32)
+                low=-np.inf, high=np.inf, shape=(7,), dtype=np.float32
+            )
         else:
             self.observation_space = spaces.Dict(
                 {
-                    "state": spaces.Box(low=-np.inf, high=np.inf, shape=(6,), dtype=np.float32),
-                    "fuel": spaces.Box(low=0, high=Chaser.initial_fuel_mass, shape=(1,), dtype=np.float32),
+                    "state": spaces.Box(
+                        low=-np.inf, high=np.inf, shape=(6,), dtype=np.float32
+                    ),
+                    "fuel": spaces.Box(
+                        low=0,
+                        high=Chaser.initial_fuel_mass,
+                        shape=(1,),
+                        dtype=np.float32,
+                    ),
                 }
             )
         # or render_mode in self.metadata['render_modes']
-        assert render_mode is None or render_mode in self.metadata['render_modes']
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
         self.render_mode = render_mode
 
         self.dmax = 100000
@@ -52,9 +66,9 @@ class Satellite_base(gym.Env):
     def reset(self):
         self.chaser = Chaser()
         state = np.zeros((6,), dtype=np.float32)
-        #state[random.randint(0,2)] = random.randint(-100, 100)
-        state[0:3] = (np.random.rand(1,3)-0.5)*10
-        state[3:] = (np.random.rand(1,3)-0.5)*1
+        # state[random.randint(0,2)] = random.randint(-100, 100)
+        state[0:3] = (np.random.rand(1, 3) - 0.5) * 10
+        state[3:] = (np.random.rand(1, 3) - 0.5) * 1
         self.chaser.set_state(state)
         self.prev_shaping = self._shape_reward()
         info = {}
@@ -65,7 +79,12 @@ class Satellite_base(gym.Env):
 
     def _get_observation(self):
         if isinstance(self.observation_space, spaces.Box):
-            return np.concatenate((self.chaser.state, np.asarray((self.chaser.fuel_mass,), dtype=np.float32)))
+            return np.concatenate(
+                (
+                    self.chaser.state,
+                    np.asarray((self.chaser.fuel_mass,), dtype=np.float32),
+                )
+            )
         else:
             return {
                 "state": self.chaser.state,
@@ -83,38 +102,42 @@ class Satellite_base(gym.Env):
         self.prev_shaping = shaping
 
         # reward -=(self.chaser.distance_to_target()+0.1)
-        reward += 1/(self.chaser.distance_to_target()+1)
+        reward += 10 / (self.chaser.distance_to_target() + 0.01)
 
         return float(reward)
 
     def _shape_reward(self):
-        shaping = (-self.chaser.distance_to_target()
-                   + 1/(self.chaser.velocity_norm()+1)
-                   + 10/(self.chaser.distance_to_target()+1))
+        shaping = (
+            -self.chaser.distance_to_target() / 10
+            # + 10/(self.chaser.velocity_norm()+1)
+            - self.chaser.fuel_mass 
+        )
         return shaping
 
     def _beyond_observational_space(self):
-        if np.any(np.abs(self.chaser.state[:3]) >= self.dmax) or np.any(np.abs(self.chaser.state[3:]) >= self.vmax):
+        if np.any(np.abs(self.chaser.state[:3]) >= self.dmax) or np.any(
+            np.abs(self.chaser.state[3:]) >= self.vmax
+        ):
             return True
         return False
-    
+
     def _action_filter(self, action):
         if self.control == "Ml":
-            action = action*Chaser.Tmax
-        #action = np.clip(action, -Chaser.Tmax, Chaser.Tmax)
-        
-        if np.any(np.abs(action)>Chaser.Tmax):
-            #remove in later versions
+            action = action * Chaser.Tmax
+        # action = np.clip(action, -Chaser.Tmax, Chaser.Tmax)
+
+        if np.any(np.abs(action) > Chaser.Tmax):
+            # remove in later versions
             print("action out of bounds")
-        
+
         return action
 
 
-class Chaser():
+class Chaser:
     mass = 30
     initial_fuel_mass = 10
-    mu = 3.986004418*1e14
-    rt = 6.6*1e6
+    mu = 3.986004418 * 1e14
+    rt = 6.6 * 1e6
     Isp = 2250
     Tmax = 1.05e-1
     g = 9.81
@@ -126,7 +149,7 @@ class Chaser():
         self.state = np.zeros((6,), dtype=np.float32)
         # self.inertia = np.array([8.33e-2,1.08e-1,4.17e-2])
         self.fuel_mass = self.initial_fuel_mass
-        self.w = np.sqrt((self.mu/(self.rt**3)), dtype=np.float32)
+        self.w = np.sqrt((self.mu / (self.rt**3)), dtype=np.float32)
         self.step = self.dt if step is None else step
 
     def set_state(self, state):
@@ -134,75 +157,102 @@ class Chaser():
         return self.state
 
     def _Sat_Translational_Dyn(self, y, t, trust):
-        dy = np.zeros(6,)
+        dy = np.zeros(
+            6,
+        )
         torques = trust.copy()
 
         dy[0] = y[3]
         dy[1] = y[4]
         dy[2] = y[5]
 
-        rc = ((self.rt+y[0])**2+y[1]**2+y[2]**2)**(1/2)
-        eta = self.mu/(rc**3)
-        total_mass = self.mass+self.fuel_mass
+        rc = ((self.rt + y[0]) ** 2 + y[1] ** 2 + y[2] ** 2) ** (1 / 2)
+        eta = self.mu / (rc**3)
+        total_mass = self.mass + self.fuel_mass
 
-        dy[3] = self.w**2*y[0]+2*self.w*y[4]+self.mu / \
-            (self.rt**2)-(eta)*(self.rt+y[0])+torques[0]/total_mass
-        dy[4] = -2*self.w*y[3]+self.w**2*y[1]-(eta)*y[1]+torques[1]/total_mass
-        dy[5] = -(eta)*y[2]+torques[2]/total_mass
+        dy[3] = (
+            self.w**2 * y[0]
+            + 2 * self.w * y[4]
+            + self.mu / (self.rt**2)
+            - (eta) * (self.rt + y[0])
+            + torques[0] / total_mass
+        )
+        dy[4] = (
+            -2 * self.w * y[3]
+            + self.w**2 * y[1]
+            - (eta) * y[1]
+            + torques[1] / total_mass
+        )
+        dy[5] = -(eta) * y[2] + torques[2] / total_mass
 
         return dy
-    
+
     def _Sat_Translational_Linear_Dyn(self, y, t, trust):
-        dy = np.zeros(6,)
+        dy = np.zeros(
+            6,
+        )
         torques = trust.copy()
 
         dy[0] = y[3]
         dy[1] = y[4]
         dy[2] = y[5]
 
-        rc = ((self.rt+y[0])**2+y[1]**2+y[2]**2)**(1/2)
-        eta = self.mu/(rc**3)
-        total_mass = self.mass+self.fuel_mass
+        rc = ((self.rt + y[0]) ** 2 + y[1] ** 2 + y[2] ** 2) ** (1 / 2)
+        eta = self.mu / (rc**3)
+        total_mass = self.mass + self.fuel_mass
 
-        dy[3] = self.w**2*y[0]+2*self.w*y[4]+self.mu / \
-            (self.rt**2)-(eta)*(self.rt+y[0])+torques[0]/total_mass
-        dy[4] = -2*self.w*y[3]+self.w**2*y[1]-(eta)*y[1]+torques[1]/total_mass
-        dy[5] = -(eta)*y[2]+torques[2]/total_mass
+        dy[3] = (
+            self.w**2 * y[0]
+            + 2 * self.w * y[4]
+            + self.mu / (self.rt**2)
+            - (eta) * (self.rt + y[0])
+            + torques[0] / total_mass
+        )
+        dy[4] = (
+            -2 * self.w * y[3]
+            + self.w**2 * y[1]
+            - (eta) * y[1]
+            + torques[1] / total_mass
+        )
+        dy[5] = -(eta) * y[2] + torques[2] / total_mass
 
         return dy
 
     def move(self, trust):
         trust = np.clip(trust, -Chaser.Tmax, Chaser.Tmax)
-        self.state = self._rk4(self._Sat_Translational_Dyn,
-                               self.state, self.dt, args=(trust,))
-        self.fuel_mass = self.fuel_mass - \
-            np.sum(np.abs(trust))/(self.g*self.Isp)*self.dt
+        self.state = self._rk4(
+            self._Sat_Translational_Dyn, self.state, self.dt, args=(trust,)
+        )
+        self.fuel_mass = (
+            self.fuel_mass - np.sum(np.abs(trust)) / (self.g * self.Isp) * self.dt
+        )
         pass
 
     def distance_to_target(self, target=np.zeros((3,), dtype=np.float32)):
-        return np.linalg.norm(self.state[0:3]-target)
+        return np.linalg.norm(self.state[0:3] - target)
 
     def velocity_norm(self):
         return np.linalg.norm(self.state[3:6])
 
     # @jit
     def _rk4(self, f, y0, t, args=()):
-        N = int(self.step/self.dt)
-        t = np.linspace(0, self.step, N+1, dtype=np.float32)
+        N = int(self.step / self.dt)
+        t = np.linspace(0, self.step, N + 1, dtype=np.float32)
         n = len(t)
         y = np.zeros((n, len(y0)), dtype=np.float32)
         y[0] = y0
-        for i in range(n-1):
-            h = t[i+1] - t[i]
+        for i in range(n - 1):
+            h = t[i + 1] - t[i]
             k1 = f(y[i], t[i], *args)
-            k2 = f(y[i] + k1 * h / 2., t[i] + h / 2., *args)
-            k3 = f(y[i] + k2 * h / 2., t[i] + h / 2., *args)
+            k2 = f(y[i] + k1 * h / 2.0, t[i] + h / 2.0, *args)
+            k3 = f(y[i] + k2 * h / 2.0, t[i] + h / 2.0, *args)
             k4 = f(y[i] + k3 * h, t[i] + h, *args)
-            y[i+1] = y[i] + (h / 6.) * (k1 + 2*k2 + 2*k3 + k4)
+            y[i + 1] = y[i] + (h / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4)
         return y[-1]
 
 
 if __name__ == "__main__":
     # Q_main()
     from stable_baselines3.common.env_checker import check_env
+
     check_env(Satellite_base(), warn=True)
--- Python/Test_env/Envs/__init__.py
+++ Python/Test_env/Envs/__init__.py
@@ -7,4 +7,13 @@ register(
 register(
     id='Satellite-v0',
     entry_point='Envs.Satellite:Satellite_base',
+    max_episode_steps=20000,
+    reward_threshold=25000.0,
+)
+register(
+    id='Satellite-discrete-v0',
+    entry_point='Envs.Satellite:Satellite_base',
+    max_episode_steps=20000,
+    reward_threshold=25000.0,
+    kwargs={'action_space': 'discrete'},
 )

39a57144dd7f45b6cc6b9dfc0921cd3985c57604
Simone Rotondi <rotondi97simone@gmail.com>
2023-05-15 23:58:07 +0200

    add ddpg

--- Python/Test_env/satellite_rot_ddpg.py
+++ Python/Test_env/satellite_rot_ddpg.py
@@ -1,14 +1,15 @@
 import matplotlib as mpl
 
 mpl.use("TkAgg")
-
 import matplotlib.pyplot as plt
 
 
-from stable_baselines3 import PPO
+from stable_baselines3 import DDPG
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.evaluation import evaluate_policy
-from Envs.Satellite_tra import Satellite_tra
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.noise import NormalActionNoise
+from Envs.Satellite_rot import Satellite_rot
 import gymnasium as gym
 import numpy as np
 import os
@@ -24,20 +25,19 @@ def fill_reward_file(imgs_dir: str):
 
     rewfile = open(f"{imgs_dir}/Reward.md", "w")
     print("```{python}", file=rewfile)
-    print(inspect.getsource(Satellite_tra._reward_function), file=rewfile)
+    print(inspect.getsource(Satellite_rot._reward_function), file=rewfile)
     print("```", file=rewfile)
 
 
 file_path = os.path.dirname(__file__)
 os.chdir(file_path)
 
-env_name = "Satellite-tra-v0"
+env_name = "Satellite-rot-v0"
 # env = gym.make(env_name)
 
-Algo = PPO
-Algo.name = "PPO"
+Algo = DDPG
+Algo.name = "DDPG"
 # ENT = 0.01
-
 use_last_model = False
 
 if use_last_model:
@@ -63,12 +63,11 @@ fill_reward_file(imgs_dir)
 
 
 def run_episode(
-    model, env_name, model_name="PPO", model_num=0, model_timesteps=0, args=()
+    model, env_name, model_name="DDPG", model_num=0, model_timesteps=0, args=()
 ):
     term = False
     env = gym.make(env_name, render_mode="rgb_array")
     obs, info = env.reset()
-
     while not term:
         action, _states = model.predict(obs)
         obs, reward, term, trunc, info = env.step(action)
@@ -77,13 +76,20 @@ def run_episode(
             plt.imsave(
                 f"{imgs_dir}/{model_name}_{model_num}_{model_timesteps:.1e}.png", X
             )
+            term = False
             break
     env.close()
 
 
-n_envs = int(os.cpu_count() / 2)
-env = make_vec_env(env_name, n_envs=n_envs)
-# env=gym.make(env_name)
+env = gym.make(env_name)
+env = Monitor(env)
+# env = gym.make(env_name)
+# add action noise for exploration
+
+n_actions = 3
+action_noise = NormalActionNoise(
+    mean=np.zeros(n_actions), sigma=0.5 * np.ones(n_actions)
+)
 
 
 TIMESTEPS = 50_000
@@ -91,19 +97,24 @@ if last_model > 0:
     model = Algo.load(
         f"{models_dir}/{Algo.name}_{last_model}",
         env=env,
+        action_noise=action_noise,
         verbose=1,
         # ent_coef=ENT,
         tensorboard_log=logdir,
     )
 else:
+    # input("Press Enter to delete logs and models")
+    # send2trash.send2trash(f"{logdir}/")
+    # send2trash.send2trash(f"{models_dir}/")
     model = Algo(
         "MlpPolicy",
         env=env,
+        action_noise=action_noise,
         verbose=1,
         # ent_coef=ENT,
         tensorboard_log=logdir,
     )
-episodes = 100
+episodes = 300
 run_episode(
     model,
     env_name=env_name,

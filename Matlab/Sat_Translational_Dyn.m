b744ffb8d8223b36cbdaf78e9b6677ea01ff9893
Simone Rotondi <rotondi97simone@gmail.com>
2023-05-30 14:13:48 +0200

    Changes to structure

--- Matlab/Sat_Translational_Dyn.m
+++ Matlab/Sat_Translational_Dyn.m
@@ -1,23 +1,23 @@
 function [dy,u] = Sat_Translational_Dyn(~,y,u) %#codegen
-
+    coder.inline('always');
     p=Sat_params();
     total_mass=(y(7)+p.mass);
-
+    
     g=9.81;
-
+    
     %Takes u directly as acc
-    u=min(p.Tmax,max(-p.Tmax,u(:)))*min(1,max(0,y(7)*1e2));
-    torques=u;
+    u=min(p.Tmax,max(-p.Tmax,u))*min(1,max(0,y(7)*1e2));
+    torques=u(:);
     y=y(:);
     dy=y;
-
+    
     %y 1to6 position and velocity
     %y 7 is mass
-
+    
     rc=((p.rt+y(1))^2+y(2)^2+y(3)^2)^(1/2);
     n=sqrt(p.mu/p.rt^3);
     eta=p.mu/(rc^3);
-        
+    
     %Init Vars using     %F_LV_LH
     dy(1)=y(4);
     dy(2)=y(5);
@@ -26,7 +26,7 @@ function [dy,u] = Sat_Translational_Dyn(~,y,u) %#codegen
     dy(5)=-2*n*y(4)+n^2*y(2)-(eta)*y(2)+torques(2)/total_mass;
     dy(6)=-(eta)*y(3)+torques(3)/total_mass;
     dy(7)=-sum(abs(u(:)))/(g*p.Isp);
-
+    
 end
 
 
--- Matlab/Sat_params.m
+++ Matlab/Sat_params.m
@@ -1,13 +1,13 @@
 
-function parameters=Sat_params()
+function parameters=Sat_params() %maybe should be a class?
     %% Sat Parameters
-
+    
     %%Satellite Motor parameters based on busek bit-1
     %satellite Isp consumption
     parameters.Isp=2250;
     %max trust
     parameters.Tmax=1.05e-5;
-
+    
     %inertia matrix, and its inverse
     %Ix = 8.33e-2;
     %Iy = 1.08e-1;
@@ -17,13 +17,13 @@ function parameters=Sat_params()
     
     parameters.mass=30;%kg
     parameters.fuel_mass=10;%kg
-
+    
     parameters.mu=3.986004418*10^14;
     parameters.rt=6.6*1e6;
     %parameters.n= 1.2e-3;%=sqrt(mu/rt^3);
-
+    
     %% when I need symbolic
-    % syms Ix Iy Iz mass Isp Tmax mu rt 
+    % syms Ix Iy Iz mass Isp Tmax mu rt
     % parameters.I=diag([Ix,Iy,Iz]);
     % parameters.invI=diag([1/Ix,1/Iy,1/Iz]);
     % parameters.mass=mass;
@@ -32,6 +32,6 @@ function parameters=Sat_params()
     % parameters.mu=mu;
     % parameters.rt=rt;
     % y=sym('y',[7 1]);u=sym('u',[3 1])
-
-
+    
+    
 end
